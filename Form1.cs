048f9241100c408536117fe604b4e00ba324ab21
LinBoSam0 <linbosam0@gmail.com>
2024-04-01 14:51:49 +0800

    4/1 1450
    改成函式

--- Form1.cs
+++ Form1.cs
@@ -51,18 +51,34 @@ namespace LengthCalculator
         // 全域變數
 string strInput; // 字串型態的strInput變數
 double douOutput; // double浮點數型態的douOutput變數
+        private void caculateAnswer(int _kind, double _value)
+        {
+            if (_kind != 0)
+                txtCM.Text = string.Format("{0:0.##########}", _value);
+            if (_kind != 1)
+                txtM.Text = string.Format("{0:0.##########}", _value / 100);
+            if (_kind != 2)
+                txtKM.Text = string.Format("{0:0.##########}", _value / 100000);
+            if (_kind != 3)
+                txtIn.Text = string.Format("{0:0.##########}", _value / 2.54);
+            if (_kind != 4)
+                txtFt.Text = string.Format("{0:0.##########}", _value / 30.48);
+            if (_kind != 5)
+                txtYard.Text = string.Format("{0:0.##########}", _value / 91.44);
+        }
 
-private void txtCM_KeyUp(object sender, EventArgs e)
+        private void txtCM_KeyUp(object sender, EventArgs e)
 {
     strInput = txtCM.Text; 
 
     if (double.TryParse(strInput, out douOutput) == true)
     {
-        txtM.Text = string.Format("{0:0.##########}", douOutput / 100);
-        txtKM.Text = string.Format("{0:0.##########}", douOutput / 100000);
-        txtIn.Text = string.Format("{0:0.##########}", douOutput / 2.54);
-        txtFt.Text = string.Format("{0:0.##########}", douOutput / 30.48);
-        txtYard.Text = string.Format("{0:0.##########}", douOutput / 91.44);
+        //txtM.Text = string.Format("{0:0.##########}", douOutput / 100);
+        //txtKM.Text = string.Format("{0:0.##########}", douOutput / 100000);
+        //txtIn.Text = string.Format("{0:0.##########}", douOutput / 2.54);
+        //txtFt.Text = string.Format("{0:0.##########}", douOutput / 30.48);
+        //txtYard.Text = string.Format("{0:0.##########}", douOutput / 91.44);
+        caculateAnswer(0, douOutput); 
     }
     else
     {
@@ -78,11 +94,12 @@ private void txtM_KeyUp(object sender, KeyEventArgs e)
      if (double.TryParse(strInput, out douOutput) == true)
             {
 
-     txtCM.Text = string.Format("{0:0.##########}", douOutput * 100);
-     txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.001);
-     txtIn.Text = string.Format("{0:0.##########}", douOutput * 39.37);
-     txtFt.Text = string.Format("{0:0.##########}", douOutput * 3.28);
-     txtYard.Text = string.Format("{0:0.##########}", douOutput * 1.09);
+     //txtCM.Text = string.Format("{0:0.##########}", douOutput * 100);
+     //txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.001);
+     //txtIn.Text = string.Format("{0:0.##########}", douOutput * 39.37);
+    // txtFt.Text = string.Format("{0:0.##########}", douOutput * 3.28);
+    // txtYard.Text = string.Format("{0:0.##########}", douOutput * 1.09);
+    caculateAnswer(1, douOutput*100); 
      }
      else
     {
@@ -98,11 +115,12 @@ private void txtM_KeyUp(object sender, KeyEventArgs e)
             if (double.TryParse(strInput, out douOutput) == true)
             {
 
-            txtCM.Text = string.Format("{0:0.##########}", douOutput * 100);
-            txtM.Text = string.Format("{0:0.##########}", douOutput * 1000);
-            txtIn.Text = string.Format("{0:0.##########}", douOutput * 39370.1);
-            txtFt.Text = string.Format("{0:0.##########}", douOutput * 3280.84);
-            txtYard.Text = string.Format("{0:0.##########}", douOutput * 1093.61);
+            //txtCM.Text = string.Format("{0:0.##########}", douOutput * 100);
+            //txtM.Text = string.Format("{0:0.##########}", douOutput * 1000);
+            //txtIn.Text = string.Format("{0:0.##########}", douOutput * 39370.1);
+            //txtFt.Text = string.Format("{0:0.##########}", douOutput * 3280.84);
+            //txtYard.Text = string.Format("{0:0.##########}", douOutput * 1093.61);
+            caculateAnswer(2, douOutput*100000); 
         }
     else
     {
@@ -118,11 +136,12 @@ private void txtM_KeyUp(object sender, KeyEventArgs e)
             if (double.TryParse(strInput, out douOutput) == true)
             {
 
-            txtCM.Text = string.Format("{0:0.##########}", douOutput * 2.54);
-            txtM.Text = string.Format("{0:0.##########}", douOutput * 0.0254);
-            txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.0000254);
-            txtFt.Text = string.Format("{0:0.##########}", douOutput * 0.0833);
-            txtYard.Text = string.Format("{0:0.##########}", douOutput * 0.0278);
+            //txtCM.Text = string.Format("{0:0.##########}", douOutput * 2.54);
+            //txtM.Text = string.Format("{0:0.##########}", douOutput * 0.0254);
+            //txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.0000254);
+            //txtFt.Text = string.Format("{0:0.##########}", douOutput * 0.0833);
+            //txtYard.Text = string.Format("{0:0.##########}", douOutput * 0.0278);
+            caculateAnswer(3, douOutput*2.54); 
          }
             else
             {
@@ -139,11 +158,12 @@ private void txtM_KeyUp(object sender, KeyEventArgs e)
             if (double.TryParse(strInput, out douOutput) == true)
             {
 
-            txtCM.Text = string.Format("{0:0.##########}", douOutput * 30.48);
-            txtM.Text = string.Format("{0:0.##########}", douOutput * 0.3048);
-            txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.0003048);
-            txtIn.Text = string.Format("{0:0.##########}", douOutput * 12);
-            txtYard.Text = string.Format("{0:0.##########}", douOutput * 0.3333);
+            //txtCM.Text = string.Format("{0:0.##########}", douOutput * 30.48);
+            //txtM.Text = string.Format("{0:0.##########}", douOutput * 0.3048);
+            //txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.0003048);
+            //txtIn.Text = string.Format("{0:0.##########}", douOutput * 12);
+            //txtYard.Text = string.Format("{0:0.##########}", douOutput * 0.3333);
+            caculateAnswer(4, douOutput*30.48);
             }
             else
             {
@@ -159,11 +179,12 @@ private void txtM_KeyUp(object sender, KeyEventArgs e)
             if (double.TryParse(strInput, out douOutput) == true)
             {
 
-            txtCM.Text = string.Format("{0:0.##########}", douOutput * 91.44);
-            txtM.Text = string.Format("{0:0.##########}", douOutput * 0.9144);
-            txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.0009144);
-            txtIn.Text = string.Format("{0:0.##########}", douOutput * 36);
-            txtFt.Text = string.Format("{0:0.##########}", douOutput * 3);
+            //txtCM.Text = string.Format("{0:0.##########}", douOutput * 91.44);
+            //txtM.Text = string.Format("{0:0.##########}", douOutput * 0.9144);
+            //txtKM.Text = string.Format("{0:0.##########}", douOutput * 0.0009144);
+            //txtIn.Text = string.Format("{0:0.##########}", douOutput * 36);
+            //txtFt.Text = string.Format("{0:0.##########}", douOutput * 3);
+            caculateAnswer(5, douOutput*91.44); 
             }
             else
             {
